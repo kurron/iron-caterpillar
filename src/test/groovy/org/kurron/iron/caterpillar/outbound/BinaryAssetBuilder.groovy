5c709b00206081e513f0b438cc8928109c58a129
Ron Kurr <kurr@kurron.org>
2015-04-26 02:13:57 +0000

    Created a custom serializer that can be read by any program.

---
+++ src/test/groovy/org/kurron/iron/caterpillar/outbound/BinaryAssetBuilder.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015. Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kurron.iron.caterpillar.outbound
+
+import org.kurron.iron.caterpillar.Builder
+import org.kurron.iron.caterpillar.Randomizer
+import org.springframework.http.MediaType
+
+/**
+ * Creates a BinaryAsset using randomized data.
+ **/
+class BinaryAssetBuilder extends Builder<BinaryAsset> {
+
+    /**
+     * Random data generator.
+     **/
+    private static final Randomizer theRandomizer = new Randomizer()
+
+    @Override
+    BinaryAsset build() {
+        new BinaryAsset().with {
+            def mediaType = MediaType.parseMediaType( "${theRandomizer.randomHexString()}/${theRandomizer.randomHexString()};${theRandomizer.randomHexString()}=${theRandomizer.randomHexString()}" )
+            contentType = mediaType.toString()
+            uploadedBy = theRandomizer.randomHexString()
+            payload = theRandomizer.randomByteArray( 32 )
+            size = payload.size()
+            it
+        }
+    }
+}
